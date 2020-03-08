ec70aeda61b34132e9d0468ea3d8fe6c11869cf0
lishuo <letwhip@gmail.com>
2020-03-08 13:50:06 +0800

    Add 移除数组重复项

--- 26-removeDuplicates.php
+++ 26-removeDuplicates.php
@@ -8,36 +8,36 @@ class Solution {
      * @param Integer[] $nums
      * @return Integer
      */
-function removeDuplicates1(&$nums) {
-    $l = count($nums);
-    $j = 0;
+    function removeDuplicates1(&$nums) {
+        $l = count($nums);
+        $j = 0;
 
-    for ($i = 1; $i < $l; $i++) {
-        if ($nums[$j] != $nums[$i]) {
-            $nums[++$j] = $nums[$i];
+        for ($i = 1; $i < $l; $i++) {
+            if ($nums[$j] != $nums[$i]) {
+                $nums[++$j] = $nums[$i];
+            }
         }
+        return ++$j;
     }
-    return ++$j;
-}
 
     /**
      * @param Integer[] $nums
      * @return Integer
      */
-function removeDuplicates(&$nums) {
-    $l = count($nums);
-    $i = 1;
-    $j = 0;
+    function removeDuplicates(&$nums) {
+        $l = count($nums);
+        $i = 1;
+        $j = 0;
 
-    while ($i < $l) {
-        if ($nums[$i] != $nums[$j]) {
-            $nums[++$j] = $nums[$i++];
-        } else {
-            $i++;
+        while ($i < $l) {
+            if ($nums[$i] != $nums[$j]) {
+                $nums[++$j] = $nums[$i++];
+            } else {
+                $i++;
+            }
         }
+        return ++$j;
     }
-    return ++$j;
-}
 }
 
 $arr = [
