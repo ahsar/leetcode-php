c20bb02139ce1213f7aa5b09c00bc406d0753361
lishuo <lishuo@100tal.com>
2021-05-07 17:25:13 +0800

    fix 正确版本快排

--- others/quickSort.php
+++ others/quickSort.php
@@ -18,32 +18,22 @@ function quickSortC(&$arr, $l, $r)
 
 function partion(&$arr, $l, $r)
 {
-    $i = $l;
-    $j = $r - 1;
+    $j = $i = $l;
     $pivot = $arr[$r];
 
-    while (1) {
-        // 右边界的数字大于标的,不换
-        while ($j > 0 && $arr[$j] > $pivot) {
-            $j--;
-        }
-
-        // 左边界数字小于边界, 不换
-        while ($i < $r && $arr[$i] < $pivot)
-            $i++;
-
-        if ($i >= $j) {
-            break;
-        }
-
-        [$arr[$i], $arr[$j]] = [$arr[$j], $arr[$i]];
+    for (; $j < $r; $j++) {
+       if ($arr[$j] < $pivot) {
+           [$arr[$i], $arr[$j]] = [$arr[$j], $arr[$i]];
+           $i++;
+       }
     }
 
     [$arr[$i], $arr[$r]] = [$arr[$r], $arr[$i]];
     return $i;
 }
 
-//$arr = [6,1,4,5,9,7,10,8];
-$arr = [3,2,3,1,2,4,5,5,6];
+$arr = [6,1,4,5,9,7,10,8];
+//$arr = [3,2,3,1,2,4,5,5,6];
+//$arr = [0,0,0, 3, 0, 6, 5];
 quickSort($arr);
 print_r($arr);
