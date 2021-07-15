bc1d833d9d259eaf041cba47c3bc1a64a13dfdd8
lishuo <lishuo@100tal.com>
2021-07-15 18:40:05 +0800

    fix 添加反转法处理旋转数组

--- 189-rotate-array.go
+++ 189-rotate-array.go
@@ -3,7 +3,9 @@
 // 双指针题型
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	var (
@@ -19,40 +21,64 @@ func main() {
 	k = 3
 
 	//case 2
-	nums = []int{-1, -100, 3, 99}
-	k = 2
-
-	//case 3
-	//nums = []int{-1}
+	//nums = []int{-1, -100, 3, 99}
 	//k = 2
 
+	//case 3
+	nums = []int{-1}
+	k = 2
+
+	//case 4
 	//nums = []int{1, 2}
+	//k = 3
+
+	//case 5
+	//nums = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	//k = 3
 
 	fmt.Println("origin: ", nums)
 	rotate(nums, k)
 }
 
-// 1, 2, 3, 4, 5, 6, 7
 /**
- * (环状替换)
- * 依次轮转替换, 直到替换次数=数组长度
+ * 翻转法
  */
 func rotate(nums []int, k int) {
+	k = k % len(nums)
+	// 整体反转
+	reverse(nums)
+
+	reverse(nums[0:k])
+	reverse(nums[k:])
+	fmt.Println(nums)
+}
+
+// 数组反转函数
+func reverse(nums []int) {
+	for i, n := 0, len(nums); i < n/2; i++ {
+		nums[i], nums[n-i-1] = nums[n-i-1], nums[i]
+	}
+}
+
+// (环状替换)
+// 依次轮转替换, 直到替换次数=数组长度
+// todo (轮转次数)
+func rotate2(nums []int, k int) {
 	length := len(nums)
 	k = k % length
 
 	var (
-		prev int     // 两两交换上一次值
-		next int     // 两两交换下一次值
-		n    int     // 记录交换次数
-		i    int = 0 // 当前索引
-		mod  int
+		prev  int     // 两两交换上一次值
+		next  int     // 两两交换下一次值
+		n     int     // 记录交换次数
+		i     int = 0 // 当前索引
+		mod   int
+		extra int = 0
 	)
 
 	// 元素的第一个元素
 	prev = nums[i]
-	for n < length {
+	for n < length+extra {
 		mod = (i + k) % length
 		// 保存被覆盖的后k位
 		next = nums[mod]
@@ -61,10 +87,13 @@ func rotate(nums []int, k int) {
 		// 此时下一个元素等于上一次循环的元素
 		prev = next
 		i += k
+
+		// 如果替换回到原始位置, 则向后移位
 		if mod == 0 {
+			//extra++
 			i++
 		}
-		fmt.Println(mod)
+		fmt.Println(i, mod, prev, nums)
 		n++
 	}
 	fmt.Println("res: ", nums)
