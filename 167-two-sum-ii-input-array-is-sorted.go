cdf92047749c33454698cda8c1813b5fe8f9c548
lishuo <lishuo@100tal.com>
2021-07-18 16:13:21 +0800

    Add 有序数组两数之和

--- 167-two-sum-ii-input-array-is-sorted.go
+++ 167-two-sum-ii-input-array-is-sorted.go
@@ -18,17 +18,39 @@ func main() {
 	target = 9
 
 	// case 2
-	nums = []int{2, 3, 4}
-	target = 6
+	//nums = []int{2, 3, 4}
+	//target = 6
 
 	// case 3
-	nums = []int{-1, 0}
-	target = -1
+	//nums = []int{-1, 0}
+	//target = -1
 
 	r = twoSum(nums, target)
 	fmt.Println(r)
 }
 
 func twoSum(numbers []int, target int) []int {
+	var (
+		l, r, sum int = 0, len(numbers) - 1, 0
+		ret       []int
+	)
+	ret = make([]int, 2, 2)
 
+	for l < r {
+		sum = numbers[l] + numbers[r]
+
+		if sum == target {
+			ret[0] = l + 1
+			ret[1] = r + 1
+			return ret
+		}
+
+		// 右指针左移
+		if sum > target {
+			r--
+		} else {
+			l++
+		}
+	}
+	return ret
 }
