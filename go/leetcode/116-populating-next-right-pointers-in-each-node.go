2be3fe6f1ff83b443d44843d5516800b46a7120a
lishuo <lishuo@100tal.com>
2021-07-25 22:55:54 +0800

    Add 116-填充每个节点的下一个右侧节点指针

---
+++ go/leetcode/116-populating-next-right-pointers-in-each-node.go
@@ -0,0 +1,21 @@
+package leetcode
+
+import "testing"
+
+func Test116(t *testing.T) {
+
+}
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Left *Node
+ *     Right *Node
+ *     Next *Node
+ * }
+ */
+
+//func connect(root *Node) *Node {
+
+//}
