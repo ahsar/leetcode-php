2159b0f2a7e58df11c7b6896d24594e4c7ec4259
lishuo <letwhip@gmail.com>
2021-07-25 11:08:48 +0800

    Add bintree create

---
+++ go/bintree/bintree.go
@@ -0,0 +1,18 @@
+package bintree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func NewRoot(v int) *TreeNode {
+	return &TreeNode{Val: v}
+}
+
+//https://juejin.cn/post/6844904145346887694
+func CreateTree(root *TreeNode, i int, arr []int) {
+	//if {
+
+	//}
+}
---
+++ go/leetcode/617-merge-two-binary-trees_test.go
@@ -0,0 +1,29 @@
+// Package leetcde provides ...
+package leetcde
+
+import (
+	"testing"
+
+	. "algo/bintree"
+)
+
+func Test617(t *testing.T) {
+	var (
+		root1, root2 *TreeNode
+	)
+
+	mergeTrees(root1, root2)
+}
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func mergeTrees(root1 *TreeNode, root2 *TreeNode) *TreeNode {
+	return root2
+}
