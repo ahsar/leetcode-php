7eb4a04090271f8afa7aaa80a11a98d07f240e05
lishuo <lishuo@100tal.com>
2021-07-29 15:47:38 +0800

    fix 正确版本的腐烂橘子(BFS)

--- go/leetcode/994-rotting-oranges_test.go
+++ go/leetcode/994-rotting-oranges_test.go
@@ -17,27 +17,33 @@ func Test994(*testing.T) {
 	// [2,1,1]
 	// [0,1,1]
 	// [1,0,1]
-	//grid = [][]int{{2, 1, 1}, {0, 1, 1}, {1, 0, 1}}
+	grid = [][]int{{2, 1, 1}, {0, 1, 1}, {1, 0, 1}}
 
 	// case 3
-	//grid = [][]int{{0, 2}}
+	grid = [][]int{{0, 2}}
 
 	// case 4
-	//grid = [][]int{{1, 2}}
+	grid = [][]int{{1, 2}}
 
 	// case 5
-	//grid = [][]int{{1}}
+	grid = [][]int{{1}}
+
+	// case 6
+	grid = [][]int{{1, 0}}
+
+	// case 7
+	grid = [][]int{{0}}
 	r := orangesRotting(grid)
 	fmt.Println(r)
 }
 
 // BFS(广度优先遍历)
 // 1. 遍历网格
-// 2. 遇到腐烂橘子, 加入队列
+// 2. 遇到橘子+1,遇到腐烂橘子, 加入队列
 // 3. while(队列不为空)
 // 4. 	一次性取出队列所有元素
 // 5. 	把腐烂橘子的四个方向依次加入队列(避免1,2 这种情况)
-// 6. 返回while 次数
+// 6. 返回while 次数(如果网格中事先存在橘子), 否则返回0
 func orangesRotting(grid [][]int) int {
 	var (
 		r, c               int = len(grid), len(grid[0])
@@ -59,6 +65,7 @@ func orangesRotting(grid [][]int) int {
 		for j := 0; j < c; j++ {
 			// 总共夺少个橘子
 			if grid[i][j] == 2 || grid[i][j] == 1 {
+				flag = true
 				orangNums++
 			}
 			if grid[i][j] == 2 {
@@ -72,7 +79,6 @@ func orangesRotting(grid [][]int) int {
 	}
 
 	for len(que) > 0 {
-		flag = true
 		que2 := que
 		que = nil
 		for _, v := range que2 {
