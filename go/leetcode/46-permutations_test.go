b91ddf479b55c7dc8854f9653efae8f2684c3937
lishuo <letwhip@gmail.com>
2021-07-31 22:43:33 +0800

    fix 全排列正确实现

--- go/leetcode/46-permutations_test.go
+++ go/leetcode/46-permutations_test.go
@@ -2,50 +2,58 @@ package leetcode
 
 // 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
 import (
+	"fmt"
 	"testing"
 )
 
 func Test46(*testing.T) {
 	var nums []int
-	// 1, 2 ,3
-	// 1, 3, 2
-	// 2, 1, 3
-	// 2, 3, 1
-	// 3, 1, 2
-	// 3, 2, 1
 
+	// case 1
 	nums = []int{1, 2, 3}
-	permute(nums)
+
+	// case 2
+	//nums = []int{0, 1}
+
+	// case 3
+	//nums = []int{1}
+
+	// case 4
+	//nums = []int{1, 8, 9, 12}
+	r := permute(nums)
+	fmt.Println(r)
 }
 
 func permute(nums []int) [][]int {
-	var ret [][]int
-	var l int = len(nums)
-	var visted map[int]int = make(map[int]int, l)
+	var (
+		ret          [][]int
+		l            int         = len(nums)
+		visted       map[int]int = make(map[int]int, l)
+		backtracking func(nums []int, l int)
+	)
 
-	// 1 2 3
-	// 1 3 2
-
-	// 3 2 1
-	// 3 1 2
-
-	// 2 3 1
-	// 2 1 3
-	backtracking := func(nums []int, l, now int) {
-		if len(path) == l {
+	backtracking = func(nums []int, l int) {
+		if len(visted) == l {
 			tmp := make([]int, l)
 			copy(tmp, path)
 			ret = append(ret, tmp)
 			return
 		}
 
-		for i := now; i < l; i++ {
-			//if {
+		for i := 0; i < l; i++ {
+			// 存在
+			if _, ok := visted[nums[i]]; ok {
+				continue
+			}
 
-			//}
+			visted[nums[i]] = i
+			push(nums[i])
+			backtracking(nums, l)
+			delete(visted, nums[i])
+			pop()
 		}
 	}
-	backtracking(nums, l, 0)
 
+	backtracking(nums, l)
 	return ret
 }
