07b33b08ecef74253a130e6fb1b7310d8ecb28d0
lishuo <letwhip@gmail.com>
2021-08-30 10:48:16 +0800

    Add 栈的最小值

---
+++ go/jindian/0302-min-stack-lcci_test.go
@@ -0,0 +1,63 @@
+// Package jindian provides ...
+package jindian
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test0302(*testing.T) {
+	var r int
+	obj := Constructor()
+	obj.Push(0)
+	obj.Push(1)
+	obj.Push(0)
+	obj.Push(-3)
+	obj.Push(3)
+	//obj.Pop()
+	////obj.Pop()
+	r = obj.GetMin()
+	obj.Pop()
+	r = obj.GetMin()
+	fmt.Println(r)
+}
+
+type MinStack struct {
+	stack    []int
+	minStack []int
+}
+
+/** initialize your data structure here. */
+func Constructor() MinStack {
+	return MinStack{}
+}
+
+func min(i, j int) int {
+	if i < j {
+		return i
+	}
+	return j
+}
+
+func (this *MinStack) Push(x int) {
+	this.stack = append(this.stack, x)
+	if len(this.minStack) <= 0 {
+		this.minStack = append(this.minStack, x)
+	} else {
+		t := this.minStack[len(this.minStack)-1]
+		this.minStack = append(this.minStack, min(x, t))
+	}
+}
+
+func (this *MinStack) Pop() {
+	this.stack = this.stack[:len(this.stack)-1]
+	this.minStack = this.minStack[:len(this.minStack)-1]
+}
+
+func (this *MinStack) Top() int {
+	return this.stack[len(this.stack)-1]
+}
+
+func (this *MinStack) GetMin() int {
+	return this.minStack[len(this.minStack)-1]
+}
