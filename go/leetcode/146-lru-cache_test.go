81482308e5bd43cff5cfa30614f774a56fa4b524
lishuo111 <lishuo111@jd.com>
2022-02-27 22:53:51 +0800

    fix(146): LRU by own
    1. write by own

--- go/leetcode/146-lru-cache_test.go
+++ go/leetcode/146-lru-cache_test.go
@@ -14,116 +14,126 @@ func Test146(*testing.T) {
 		val      int
 	)
 
-	capacity = 2
+	capacity = 3
 
 	obj := Constructor(capacity)
 	obj.Put(1, 1)
 	obj.Put(2, 2)
-	obj.Put(3, 3)
+	obj.Put(1, 3)
+	obj.Put(4, 4)
+	//obj.Put(5, 5)
 	val = obj.Get(1)
 	fmt.Println(val)
 }
 
+/**
+ * 实现方式
+ *
+ * hashmap + Doubly linked list
+ * (dummy head, tail)
+ *
+ */
+
+type LRUNode struct {
+	prev     *LRUNode
+	next     *LRUNode
+	key, val int
+}
+
 type LRUCache struct {
-	indexMap       map[int]*LruNode
-	head           *LruNode
-	tail           *LruNode
-	capacity, size int
+	hash           map[int]*LRUNode
+	head, tail     *LRUNode
+	capacity, used int
 }
 
-type LruNode struct {
-	K, V int
-	Prev *LruNode
-	Next *LruNode
-}
-
-// 创造一个tail->head 的双向链表(head 为最近使用)
 func Constructor(capacity int) LRUCache {
-	head := &LruNode{}
-	tail := &LruNode{}
-	head.Prev = tail
-	tail.Next = head
+	tail := &LRUNode{key: -2, val: -2}
+	head := &LRUNode{key: -3, val: -3}
+	tail.next = head
+	head.prev = tail
 
 	return LRUCache{
-		head:     head,
+		hash:     make(map[int]*LRUNode, capacity),
 		tail:     tail,
+		head:     head,
 		capacity: capacity,
-		indexMap: make(map[int]*LruNode),
+		used:     0,
 	}
 }
 
+/**
+ * 1. map key to *node
+ * 2. move *node to head
+ * 3. relink old link
+ */
 func (this *LRUCache) Get(key int) int {
 	var (
-		r    int = -1
-		node *LruNode
+		node *LRUNode
 		ok   bool
 	)
 
-	// 不存在
-	if node, ok = this.indexMap[key]; !ok {
+	// not exists return
+	if node, ok = this.hash[key]; !ok {
 		return -1
 	}
 
-	// 存在
-	// 1. 找到位置(hash)
-	// 2. 移动到链表头部节点
-	r = node.V
-
-	this.PutToHead(node)
-	return r
+	this.move2head(node)
+	return node.val
 }
 
+/**
+ * 1. new *node
+ * 2. node 2 head
+ * 3. if len > cap
+ * 3.1 tail to tail prev
+ * 3.2 release old node
+ */
 func (this *LRUCache) Put(key int, value int) {
-	// 判断是否存在
 	var (
-		node *LruNode
+		node *LRUNode
 		ok   bool
 	)
+	if node, ok = this.hash[key]; !ok {
+		node = &LRUNode{key: key}
+	}
+	node.val = value
+	this.hash[key] = node
 
-	if node, ok = this.indexMap[key]; ok {
-		// 存在更新value
-		node.V = value
-	} else {
-		// 不存在 新建一个节点
-		node = &LruNode{
-			K:    key,
-			V:    value,
-			Prev: nil,
-			Next: nil,
-		}
-		this.indexMap[key] = node
-		this.size++
+	if !ok {
+		this.used++
 	}
 
-	this.PutToHead(node)
-
-	// 容量是否超出容量
-	if this.size > this.capacity {
-		// 删除链表最后元素
-		last := this.tail.Next
-		// tail cur next
-		last.Next.Prev = this.tail
-		this.tail.Next = last.Next
-		delete(this.indexMap, last.K)
-		last = nil
-		this.size--
+	this.move2head(node)
+	if this.used <= this.capacity {
+		return
 	}
+
+	// 删除链表最后节点
+	tail := this.tail.next
+
+	last2 := this.tail.next.next
+	this.tail.next = last2
+	last2.prev = this.tail
+
+	delete(this.hash, tail.key)
+	this.used--
 }
 
-func (this *LRUCache) PutToHead(node *LruNode) {
-	// 把当前节点从原有链表位置剔除
-	if node.Next != nil && node.Prev != nil {
-		next := node.Next
-		prev := node.Prev
-		prev.Next = next
-		next.Prev = prev
+/**
+ * 把一个节点从原来的位置移动到链表头指针
+ */
+func (this *LRUCache) move2head(node *LRUNode) {
+	// 新创建的节点不需要从末端移动
+	if node.prev != nil && node.next != nil {
+		prev := node.prev
+		next := node.next
+		prev.next = next
+		next.prev = prev
 	}
 
-	// 节点放到链表头部(最近使用)
-	//tail a b c head
-	head := this.head.Prev
-	head.Next = node
-	node.Prev = head
-	node.Next = this.head
-	this.head = node
+	head := this.head.prev
+	head.next = node
+	node.prev = head
+	this.head.prev = node
+	node.next = this.head
 }
